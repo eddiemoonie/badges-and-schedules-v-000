9ca9670b718c919dddb00ecbb6a5aa25fba237d5
Eddie Moon <eddiemoonie@gmail.com>
2018-02-13 22:51:34 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,3 +1,10 @@
+attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
 def badge_maker(name)
   return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  attendees.each do |name|
+    "Hello, my name is #{name}."
 end
